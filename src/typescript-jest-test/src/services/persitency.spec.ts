24668e66e6802d3f8cdf94f7fe8c6a0ae7098f32
antoniomrrds <arqueiroverd@gmail.com>
2022-10-16 18:47:42 -0300

    typescript jest initial

---
+++ src/typescript-jest-test/src/services/persitency.spec.ts
@@ -0,0 +1,13 @@
+describe('one test', () => {
+  it('should return one', () => {
+    const number = 1;
+    expect(number).toBe(1);
+  });
+});
+
+describe('Other test', () => {
+  it('should return the name Antonio', () => {
+    const name = 'Antonio';
+    expect(name).toBe('Antonio');
+  });
+});
