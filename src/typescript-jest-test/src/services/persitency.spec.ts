f4a52d033a0e67be068c80032c6aa1ac85c0f372
antoniomrrds <arqueiroverd@gmail.com>
2022-10-17 16:34:56 -0300

    First test with spyOn 'jest spyOn'

--- src/typescript-jest-test/src/services/persitency.spec.ts
+++ src/typescript-jest-test/src/services/persitency.spec.ts
@@ -1,13 +1,30 @@
-describe('one test', () => {
-  it('should return one', () => {
-    const number = 1;
-    expect(number).toBe(1);
-  });
-});
+import { Persistency } from './persistency';
 
-describe('Other test', () => {
-  it('should return the name Antonio', () => {
-    const name = 'Antonio';
-    expect(name).toBe('Antonio');
+describe('Persinstency', () => {
+  //limpa apos cada teste
+  afterEach(() => jest.clearAllMocks());
+
+  it('should return undefined', () => {
+    //System under test
+    const sut = new Persistency();
+    expect(sut.saveOrder()).toBeUndefined();
+  });
+  it('should call  console.log once', () => {
+    //System under test
+
+    const sut = new Persistency();
+    const consoleSpy = jest.spyOn(console, 'log');
+    sut.saveOrder();
+    expect(consoleSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call  console.log with "Pedido salvo com sucesso ...', () => {
+    //System under test
+
+    const sut = new Persistency();
+    const consoleSpy = jest.spyOn(console, 'log');
+    sut.saveOrder();
+    //chamando o metodo e comparando se o valor e igual ao pretendido
+    expect(consoleSpy).toHaveBeenCalledWith('Pedido salvo com sucesso...');
   });
 });
