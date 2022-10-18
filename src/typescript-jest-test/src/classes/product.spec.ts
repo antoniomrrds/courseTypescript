1de9ccb10a4f89a03e827e7e0ab0b051777f788f
antoniomrrds <arqueiroverd@gmail.com>
2022-10-18 03:58:38 -0300

    test Product

---
+++ src/typescript-jest-test/src/classes/product.spec.ts
@@ -0,0 +1,17 @@
+import { Product } from './product';
+
+
+const createSut = (name:string,price:number ): Product => new Product(name,price);
+
+describe('Messaging', () => {
+  //limpa apos cada teste
+  afterEach(() => jest.clearAllMocks());
+
+  it('should return name and price correct', () => {
+    //System under test
+    const sut = createSut('caneta',23.50);
+    expect(sut).toHaveProperty('name','caneta');
+    expect(sut).toHaveProperty('price',23.50);
+    expect(sut.price).toBeCloseTo(23.50);
+  });
+});
